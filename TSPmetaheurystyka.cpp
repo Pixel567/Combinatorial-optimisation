a6c823beff6aba26685bf501b1001bb7c26bc7c0
ErykZinkowski <161568621+ErykZinkowski@users.noreply.github.com>
2024-11-23 14:18:18 +0100

    Add files via upload

--- TSPmetaheurystyka.cpp
+++ TSPmetaheurystyka.cpp
@@ -13,18 +13,23 @@ struct points
 	int X;
 	int Y;
 };
-void macierzfill(int n,int ** macierz,points * list)
+struct path
+{
+	float feromony;
+	int distance;
+};
+void macierzfill(int n,path ** macierz,points * list)
 {
 	for(int i=1;i<=n;i++)
 		for(int j=1;j<i;j++)
 		{
 			if(i!=j)
 			{
-				macierz[i][j]=sqrt(pow(list[i].X-list[j].X,2)+pow(list[i].Y-list[j].Y,2));
+				macierz[i][j].distance=sqrt(pow(list[i].X-list[j].X,2)+pow(list[i].Y-list[j].Y,2));
 			}
 		}
 }
-void ant(int n,int ** macierz)
+void ant(int n,path ** macierz)
 {
 	
 }
@@ -42,12 +47,12 @@ int main()
 	}
 	plik.close();
 	//for(int i=1;i<=n;i++) cout<<i<<" "<<list[i].X<<" "<<list[i].Y<<endl;//wypisuje wczytane dane 
-	int ** macierz;
-	macierz = new int * [n+1];
-	for (int i = 1; i <= n; i++) macierz[i] = new int[n+1];
-	for(int i=0;i<=n;i++) for(int j=0;j<=n;j++) macierz[i][j]=0;
+	path ** macierz;
+	macierz = new path * [n+1];
+	for (int i = 1; i <= n; i++) macierz[i] = new path[n+1];
+	for(int i=0;i<=n;i++) for(int j=0;j<=n;j++) macierz[i][j].distance=0;
 	macierzfill(n,macierz,list);
-	for(int i=1;i<=n;i++) for(int j=1;j<=i;j++) macierz[j][i]=macierz[i][j];
-	for(int i=1;i<=n;i++) {for(int j=1;j<=n;j++) cout<<macierz[i][j]<<" "; cout<<endl;}//wypisuje macierz
+	for(int i=1;i<=n;i++) for(int j=1;j<=i;j++) macierz[j][i].distance=macierz[i][j].distance;
+	for(int i=1;i<=n;i++) {for(int j=1;j<=n;j++) cout<<macierz[i][j].distance<<" "; cout<<endl;}//wypisuje macierz odleg³oœci
 	return 0;
 }
