d16437ef812f74b20b840938bf4cf25fb1d31f82
ErykZinkowski <161568621+ErykZinkowski@users.noreply.github.com>
2024-12-08 16:27:52 +0100

    Add files via upload

--- TSPzachłanny.cpp
+++ TSPzachłanny.cpp
@@ -19,28 +19,32 @@ struct points
 
 void greedy(int n,float ** macierz) //algorytm zach�anny dla TSP
 {
+	float length=0.0;
 	cout<<endl;
-	int min[2];
-	min[0]=0;
-	int i=0;
-	while(min[0]!=N)
+	int min;
+	float min0=0;
+	int i=1;
+	for(int j=1;j<=n;j++){macierz[0][j]=macierz[1][j];}
+	while(min0!=N)
 	{
 		cout<<i<<" ";
-		min[0]=N;
-		for(int j=0;j<n;j++)
+		min0=N;
+		for(int j=1;j<=n;j++)
 		{
-			if(macierz[i][j]<min[0] && macierz[i][j]!=0)
+			if(macierz[i][j]<min0 && macierz[i][j]!=0)
 			{
-				min[0]=macierz[i][j];
-				min[1]=j;
+				min0=macierz[i][j];
+				min=j;
 			}
 		}
-		macierz[i][min[1]]=0;
-		for(int k=0;k<n;k++) macierz[k][i]=0;
-		i=min[1];
-		if(min[0]==N) {i=0; cout<<i<<endl;} //ostatni przypadek powr�t do pocz�tku
+		macierz[i][min]=0;
+		for(int k=1;k<=n;k++) macierz[k][i]=0;
+		if(min0==N) {length+=macierz[0][i]; i=1; cout<<i<<endl;} //ostatni przypadek powr�t do pocz�tku
+		length+=min0;
+		i=min;
 		//for(int i=0;i<n;i++) {for(int j=0;j<n;j++) cout<<macierz[i][j]<<" "; cout<<endl;} cout<<endl; // wy�wietlanie zmian w tablicy
 	}
+	cout<<endl<<"Length: "<<length<<endl;
 }
 void matrixfill(int n,float ** macierz,points * list) //calulates distance from coordinates
 {
@@ -58,7 +62,7 @@ int main()
 	int n;
 	srand(time(NULL));//przygotowanie generatora liczb pseudolosowych
 	ifstream plik;
-	plik.open("dane.txt");
+	plik.open("dane2.txt");
 	plik>>n;
 	points * list = new points[n+1];
 	for(int i=1;i<=n;i++)
@@ -71,9 +75,9 @@ int main()
 	for (int i = 0; i <= n; i++) macierz[i] = new float[n+1];
 	for(int i=0;i<=n;i++) for(int j=0;j<=n;j++) macierz[i][j]=0;
 	//for(int i=0;i<n;i++) for(int j=0;j<i;j++) macierz[i][j]=rand()%9+1;
-	for(int i=0;i<=n;i++) for(int j=0;j<=i;j++) macierz[j][i]=macierz[i][j];
-	//for(int i=0;i<n;i++) {for(int j=0;j<n;j++) cout<<macierz[i][j]<<" "; cout<<endl;}
 	matrixfill(n,macierz,list);
+	for(int i=0;i<=n;i++) for(int j=0;j<=i;j++) macierz[j][i]=macierz[i][j];
+	for(int i=0;i<=n;i++) {for(int j=0;j<=n;j++) cout<<macierz[i][j]<<" "; cout<<endl;}
 	greedy(n,macierz);
 	for (int i = 0; i <= n; i++) {
         delete[] macierz[i];
